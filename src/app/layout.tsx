5f557f29e6262d6db789e412132dd3dadd7a7c45
PolCarva <pablocarvalhogimenez@gmail.com>
2024-09-28 17:06:14 -0300

    Add Tailwind CSS configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from './_providers'
 import { InitTheme } from './_providers/Theme/InitTheme'
 import { mergeOpenGraph } from './_utilities/mergeOpenGraph'
 
+import './_css/global.scss'
 import './_css/app.scss'
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
